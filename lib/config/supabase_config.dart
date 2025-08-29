1c8bbc239f669f28241e1ddccbaa13ae06c32396
Dwi Aji Cahyono <dwiaji724@gmail.com>
2025-08-29 18:39:06 +0700

    update hilangin config

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -1,9 +1,8 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseConfig {
-  static const String url = 'https://zwhwdzispxviuzmxodyd.supabase.co/';
-  static const String anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3aHdkemlzcHh2aXV6bXhvZHlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0ODAyMTcsImV4cCI6MjA2NjA1NjIxN30.3plM9D0Rp3w2Jr1I0iGSGgMr4tzrDAXHf0WiXQZVTCI';
+  static const String url = '';
+  static const String anonKey = '';
 
   static Future<void> initialize() async {
     await Supabase.initialize(url: url, anonKey: anonKey);
